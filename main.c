b19877398e8d880e3c1098e9cbbe3cee4a7a13f4
chopster44 <oscar@chopster44.com>
2024-02-01 18:01:12 +0000

    added random number test

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void randomNumber() {
+  int rand = random();
+  printf("%d\n", rand);
+  return;
+}
+
 int main(int argc, char *argv[]) {
   char* ptr = malloc(sizeof(char) * 10);
   printf("Enter up to 10 chars");
@@ -8,5 +14,6 @@ int main(int argc, char *argv[]) {
   printf("%s \n", ptr);
   printf("Hello, World!");
   free(ptr);
+  randomNumber();
   return 0;
 }

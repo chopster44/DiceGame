daf08c281d31f6f19f6a9b38f721cb3988cc7a66
chopster44 <62182317+chopster44@users.noreply.github.com>
2024-02-05 09:54:51 +0000

    Game start

--- src/main.c
+++ src/main.c
@@ -3,35 +3,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-void randomNumber() {
-  int rand = random();
-  printf("%d\n", rand);
-  return;
+void randomNumber()
+{
+    int rand = random();
+    printf("%d\n", rand);
+    return;
 }
 
 void cls() { system("clear"); }
 
-int main(int argc, char *argv[]) {
-  cls();
-  char *players[2] = {malloc(sizeof(char) * 3), malloc(sizeof(char) * 3)};
-  printf("Welcome to Dice game!\n");
-  printf("Who is playing?\n Player 1: ");
-  login(0, players);
-  printf("Player2: ");
-  login(1, players);
-  printf("Player1: %s, Player2: %s\n", players[0], players[1]);
-  printf("Begin? ");
-  char *temp = malloc(sizeof(char));
-  scanf("%s", temp);
-  if (strcmp(temp, "")) {
+int main(int argc, char *argv[])
+{
+    cls();
+    char *players[2] = {malloc(sizeof(char) * 3), malloc(sizeof(char) * 3)};
+    int *score[2] = {malloc(sizeof(int)), malloc(sizeof(int))};
+    printf("Welcome to Dice game!\n");
+    printf("Who is playing?\n Player 1: ");
+    login(0, players);
+    printf("Player2: ");
+    login(1, players);
+begin:
+    printf("Player1: %s, Player2: %s\n", players[0], players[1]);
+    printf("Begin? [y/n] ");
+    char *temp = malloc(sizeof(char));
+    scanf("%s", temp);
+    if (!strcmp(temp, "n"))
+    {
+        printf("Quiting\n");
+        free(temp);
+        free(players[0]);
+        free(players[1]);
+        return 0;
+    }
+    else if (!strcmp(temp, "y"))
+    {
+        goto game;
+    }
+    else
+    {
+        printf("Unknown char, try again\n");
+        goto begin;
+    }
+game:
+    cls();
+    // player 1 roll
+    free(temp);
     free(players[0]);
     free(players[1]);
-    free(temp);
     return 0;
-  }
-  cls();
-  free(temp);
-  free(players[0]);
-  free(players[1]);
-  return 0;
 }
